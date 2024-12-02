f3b08def5319828b83c32a77976e83a13014af73
Stefan Poindl <stefan@tohuwabohu.io>
2024-12-02 16:07:31 +0100

    add day01 part02

--- src/main/java/io/tohuwabohu/Main.java
+++ src/main/java/io/tohuwabohu/Main.java
@@ -4,10 +4,18 @@ import io.tohuwabohu.aoc2024.day01.Day01;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Advent of Code 2024");
+        System.out.println("===================");
+
         try {
+            System.out.println("----- Day 01 ------");
+
             Day01 day01 = new Day01();
 
+            System.out.print("Part 01:\t");
             System.out.println(day01.part1("src/main/resources/day01.txt"));
+            System.out.print("Part 02:\t");
+            System.out.println(day01.part2("src/main/resources/day01.txt"));
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/io/tohuwabohu/aoc2024/day01/Day01.java
+++ src/main/java/io/tohuwabohu/aoc2024/day01/Day01.java
@@ -3,7 +3,9 @@ package io.tohuwabohu.aoc2024.day01;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Day01 {
@@ -25,6 +27,33 @@ public class Day01 {
 
         for (int i = 0; i < leftList.size(); i++) {
             result += Math.abs(leftList.get(i) - rightList.get(i));
+        }
+
+        return result;
+    }
+
+    public long part2(String fileName) throws FileNotFoundException {
+        var bufferedReader = new BufferedReader(new FileReader(fileName));
+
+        var splitLists = bufferedReader.lines()
+                .map(line -> line.split(" {3}"))
+                .collect(Collectors.teeing(
+                        Collectors.mapping(split -> Integer.parseInt(split[0]), Collectors.toList()),
+                        Collectors.mapping(split -> Integer.parseInt(split[1]), Collectors.toList()),
+                        List::of
+                ));
+
+        var leftList = new ArrayList<>(splitLists.get(0).stream().sorted().toList());
+
+        var rightList = new ArrayList<>(splitLists.get(1).stream().sorted().toList());
+        rightList.retainAll(leftList);
+
+        leftList.retainAll(rightList);
+
+        var result = 0;
+
+        for (Integer search : leftList) {
+            result += search * (int) rightList.stream().filter(rightItem -> Objects.equals(rightItem, search)).count();
         }
 
         return result;
